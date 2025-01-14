ac11dd126d5869a89fd96fc8fe08656634b7cf11
ihjas-1 <ihjas.one@gmail.com>
2025-01-14 16:25:06 +0530

    extra

--- day2/example1/src/components/Home.jsx
+++ day2/example1/src/components/Home.jsx
@@ -1,13 +1,18 @@
-import { Stack } from '@mui/material'
-import React from 'react'
+import { Button, Stack, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
+import { Link } from 'react-router'
 
 
 const Home = () => {
+    var [name,setName] = useState("Ihjas")
+
   return (
     <>
         <Stack padding={0} margin={-1}>
             <NavBar title={"Home"} />
+            
+            <Typography width={"100vw"} textAlign={"center"}>Welcome {name}</Typography>
         </Stack>
     </>
   )
--- day2/example1/src/components/NavBar.jsx
+++ day2/example1/src/components/NavBar.jsx
@@ -1,14 +1,22 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router'
 
-const NavBar = ({title}) => {
+const NavBar = ({ title }) => {
     return (
         <AppBar position="static">
             <Toolbar variant="dense">
                 <Typography variant="h6" color="inherit" component="div">
                     {title}
+
                 </Typography>
-                
+            
+                <Link to="login">
+                    <Button color='green'>Login</Button>
+                </Link>
+                <Link to="signup">
+                    <Button color='green'>Sign Up</Button>
+                </Link>
             </Toolbar>
         </AppBar>
     )
